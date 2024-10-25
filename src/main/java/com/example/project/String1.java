b4428e267f132e88a450782ac98dd01ab760405d
nasse <nasse@N-HP>
2024-10-25 13:21:44 +0300

    commit

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -4,35 +4,54 @@ public class String1 {
 	private char[] str;
 	private int size;
 
-	public String1 () {
-	   size = -1;
-	   str = new char[80];
+	public String1() {
+		size = -1;
+		str = new char[80];
 	}
+
 	public void append(char c) {
-        str[++size] = c;
-    }
-    public char remove() {
-        return str[size--];
-    }
-    public char getChar(int i) {
-        return str[i];
-    }
-    public int length() {
-        return size+1;
-    }
-    public void makeEmpty()
-    {
-        size = -1;
-    }
-    public void concat(String1 s)
-    {
-        for(int i = 0; i < s.length(); i++)
-            append(s.getChar(i));
-    }
-    public boolean equal(String1 s) {
-        throw new UnsupportedOperationException("equal() function is not supported yet.");
-    }
-    public void reverse() {
-        throw new UnsupportedOperationException("reverse() function is not supported yet.");
-    }
+		str[++size] = c;
+	}
+
+	public char remove() {
+		return str[size--];
+	}
+
+	public char getChar(int i) {
+		return str[i];
+	}
+
+	public int length() {
+		return size + 1;
+	}
+
+	public void makeEmpty() {
+		size = -1;
+	}
+
+	public void concat(String1 s) {
+		for (int i = 0; i < s.length(); i++)
+			append(s.getChar(i));
+	}
+
+	public boolean equal(String1 s) {
+		if (s.length() == 0 || str.length == 0 || s.length() != str.length)
+			return false;
+		for (int i = 0; i < s.length(); i++) {
+			if (!(str[i] == s.getChar(i)))
+				return false;
+		}
+		return true;
+	}
+
+	public void reverse() {
+		String1 NewString = new String1();
+		int Newsize = (this.size) - 1;
+
+		for (int i = 0; i < size; i++)
+			NewString.append(str[Newsize--]);
+
+		for (int i = 0; i < size; i++)
+			str[i] = NewString.getChar(i);
+	}
 }
